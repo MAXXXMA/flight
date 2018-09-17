9356c4a8adfe34b20a2a89e49b80edad18e78fef
Max Ma <maxmauts@gmail.com>
2018-09-17 16:18:46 +1000

    add more flight

--- src/java/bootstrap/BootstrapData.java
+++ src/java/bootstrap/BootstrapData.java
@@ -18,6 +18,8 @@ import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,8 +35,8 @@ public class BootstrapData {
         }
         return null;
     }
-    
-    public static void main(String args[]) {
+
+    public static void main(String args[]) throws ParseException {
 
         UserDao userDao = new UserDao();
         FlightDao flightDao = new FlightDao();
@@ -43,29 +45,37 @@ public class BootstrapData {
         userDao.save(new Users());
         flightDao.save(new Flights());
         bookingDao.save(new Bookings());
-        
+
         List<User> users = new ArrayList<User>();
         users.add(new User("user1@uts.com", "user1", "Tom"));
         users.add(new User("user2@uts.com", "user2", "Bob"));
         users.add(new User("user3@uts.com", "user3", "Jim"));
         users.add(new User("user4@uts.com", "user4", "Jerry"));
         users.add(new User("user5@uts.com", "user5", "Alice"));
-        
-        List<Flight> flights = new ArrayList<Flight>();
-        flights.add(new Flight("QF101", "Sydney", "Melbourne", toDate("2018-09-21 08:00:00"), toDate("2018-09-21 10:00:00"), 100, 20, 99, 199));
-        flights.add(new Flight("QF102", "Melbourne", "Sydney", toDate("2018-09-21 13:00:00"), toDate("2018-09-21 15:00:00"), 100, 20, 99, 199));
-        flights.add(new Flight("QF101", "Sydney", "Melbourne", toDate("2018-09-22 08:00:00"), toDate("2018-09-22 10:00:00"), 100, 20, 49, 109));
-        flights.add(new Flight("QF102", "Melbourne", "Sydney", toDate("2018-09-22 13:00:00"), toDate("2018-09-22 15:00:00"), 100, 20, 49, 109));
-        
-        flights.add(new Flight("QF201", "Sydney", "Cairns", toDate("2018-09-21 14:00:00"), toDate("2018-09-21 17:00:00"), 80, 10, 199, 399));
-        flights.add(new Flight("QF202", "Cairns", "Sydney", toDate("2018-09-21 18:00:00"), toDate("2018-09-21 21:00:00"), 80, 10, 199, 399));
-        flights.add(new Flight("QF201", "Sydney", "Cairns", toDate("2018-09-22 14:00:00"), toDate("2018-09-22 17:00:00"), 80, 10, 249, 599));
-        flights.add(new Flight("QF202", "Cairns", "Sydney", toDate("2018-09-22 18:00:00"), toDate("2018-09-22 21:00:00"), 80, 10, 249, 599));
 
-        flights.add(new Flight("QF301", "Sydney", "Gold Coast", toDate("2018-09-21 11:00:00"), toDate("2018-09-21 13:00:00"), 60, 20, 89, 149));
-        flights.add(new Flight("QF302", "Gold Coast", "Sydney", toDate("2018-09-21 14:00:00"), toDate("2018-09-21 16:00:00"), 60, 20, 99, 199));
-        flights.add(new Flight("QF301", "Sydney", "Gold Coast", toDate("2018-09-22 11:00:00"), toDate("2018-09-22 13:00:00"), 60, 20, 139, 499));
-        flights.add(new Flight("QF302", "Gold Coast", "Sydney", toDate("2018-09-22 14:00:00"), toDate("2018-09-22 16:00:00"), 60, 20, 129, 449));
+        List<Flight> flights = new ArrayList<Flight>();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date start = formatter.parse("2018-09-21");
+        Date end = formatter.parse("2018-10-21");
+
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(start);
+
+        Calendar endCalendar = new GregorianCalendar();
+        endCalendar.setTime(end);
+        while (calendar.before(endCalendar)) {
+            Date now = calendar.getTime();
+            String day = formatter.format(now);
+            flights.add(new Flight("QF101", "Sydney", "Melbourne", toDate(day + " 08:00:00"), toDate(day + " 10:00:00"), 100, 20, 99, 199));
+            flights.add(new Flight("QF102", "Melbourne", "Sydney", toDate(day + " 13:00:00"), toDate(day + " 15:00:00"), 100, 20, 99, 199));
+            flights.add(new Flight("QF201", "Sydney", "Cairns", toDate(day + " 14:00:00"), toDate(day + " 17:00:00"), 80, 10, 199, 399));
+            flights.add(new Flight("QF202", "Cairns", "Sydney", toDate(day + " 18:00:00"), toDate(day + " 21:00:00"), 80, 10, 199, 399));
+            flights.add(new Flight("QF301", "Sydney", "Gold Coast", toDate(day + " 11:00:00"), toDate(day + " 13:00:00"), 60, 20, 89, 149));
+            flights.add(new Flight("QF302", "Gold Coast", "Sydney", toDate(day + " 14:00:00"), toDate(day + " 16:00:00"), 60, 20, 99, 199));
+
+            calendar.add(Calendar.DATE, 1);
+        }
 
         List<Booking> bookings = new ArrayList<Booking>();
 
@@ -73,15 +83,15 @@ public class BootstrapData {
         bookings.add(new Booking(flights.get(4).getFlightId(), users.get(0).getUserId(), toDate("2018-09-06 08:00:00"), "Economic", 1, flights.get(4).getSeatPrice() * 1));
         bookings.add(new Booking(flights.get(8).getFlightId(), users.get(0).getUserId(), toDate("2018-09-07 08:00:00"), "Economic", 1, flights.get(8).getSeatPrice() * 2));
 
-        for(User user: users) {
+        for (User user : users) {
             userDao.add(user);
         }
 
-        for(Flight flight: flights) {
+        for (Flight flight : flights) {
             flightDao.add(flight);
         }
 
-        for(Booking booking: bookings) {
+        for (Booking booking : bookings) {
             bookingDao.add(booking);
         }
 
